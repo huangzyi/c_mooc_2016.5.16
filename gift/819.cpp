00ab4c9234598f8ae505a2e6d7b20c2e92a11c2a
huangzyi <huangzyi@163.com>
2016-05-14 22:32:00 +0800

    3
    老师的方法，只有第一小题，输入法抽了，所以没写注释

--- gift/819.cpp
+++ gift/819.cpp
@@ -1,4 +1,76 @@
 #include <stdio.h>
+#define N 3
+#define M 3
+int ave(int a[M+1]);
+int line(int b[][N]);
+int main()
+{
+	int a[N][M+1]={0},i,j,b[3][N]={0};
+
+
+	for(i=0;i<N;i++)
+	{
+	printf("第%d人成绩：\n",i+1);
+	for(j=1;j<=M;j++)
+	{
+		scanf("%d",&a[i][j]);
+		getchar();
+	}
+	ave(a[i]);
+	printf("");
+	b[1][i]=a[i][0];
+	b[0][i]=i;
+	}
+
+
+	 line(b);
+	for(i=0;i<N;i++)
+		b[2][i]=i;
+		for(i=0;i<N;i++)
+		{
+			if(i>0)
+			{
+			if(b[1][i]==b[1][i-1])
+				b[2][i]=b[2][i-1];
+			}
+				printf("第%d名为%d号选手 %d分\n",b[2][i]+1,b[0][i]+1,b[1][i]);
+		}
+	return 0;
+}
+int ave(int a[M+1])
+{
+	int i,max,min;
+	max=a[1];min=a[1];
+	for(i=1;i<=M;i++)
+	{
+		if(a[i]>max) max = a[i];
+		if(a[i]<min) min = a[i];
+		a[0]+=a[i];
+	}
+	a[0]-=max+min;
+	a[0]/=M-2;
+	return 0;
+}
+int line(int b[][N])
+{
+	int i,j,t,d;
+	for(i=0;i<N-1;i++)
+	{
+		for(j=1;j<N;j++)
+		{
+			if(b[1][i]<b[1][j]) 
+			{
+				t=b[1][i];			d=b[0][i];
+				b[1][i]=b[1][j];	b[0][i]=b[0][j];
+				b[1][j]=t;			b[0][j]=d;
+			}
+		}
+	}
+	return 0;
+}
+
+/*
+#include <stdio.h>
 #include <math.h>
 #define N 4
 #define M 4
@@ -97,3 +169,4 @@ float line(float b[])
 	 printf("\n");
 	return 0;
 }
+*/
