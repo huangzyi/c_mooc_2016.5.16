b390e5a7155f96473ee161628ae080b08be39a45
huangzyi <huangzyi@163.com>
2016-05-14 23:56:02 +0800

    只有第一小题
    用老师的方法写的，类型定义错误，慎入，编辑第二小题时会遇到深坑，当然如果你们有办法看我往期版本说不定能找到我写的正确答案哦

--- gift/819.cpp
+++ gift/819.cpp
@@ -8,6 +8,7 @@ int main()
 {
 	//初定义变量，将数组初始化为0
 	//定义a的二维数组存储选手成绩;b为a排序后的数组，b[0]存储选手序号，b[1]存储选手分数，b[2]存储选手名次
+	//类型最好改为double
 	int a[N][M+1]={0},i,j,b[3][N]={0};
 
 	//输入选手成绩，a[i]的存入第i-1位选手的成绩，第j位评委的分数存入a[i][j]中，将计算后的分数存在a[i][0]中
@@ -91,104 +92,3 @@ int line(int b[][N])
 	return 0;
 }
 
-/*
-#include <stdio.h>
-#include <math.h>
-#define N 4
-#define M 4
-float ave(int a[M]);
-float line(float b[]);
-int main()
-{
-	int a[N][M]={0},i,j,find=0;
-	float b[N],c[N],d[N]={0},e[N]={0};
-	for(i=0;i<N;i++)
-	{
-	printf("第%d人成绩：\n",i+1);
-	for(j=0;j<M;j++)
-	{
-		scanf("%d",&a[i][j]);
-		getchar();
-	}
-	b[i]=c[i]=ave(a[i]);
-	printf("最终：	%3.2f\n",c[i]);
-	for(j=0;j<M;j++)
-		d[j]+=fabs(a[i][j]-c[i]);
-	}
-		for(j=0;j<M;j++)
-		e[j]=d[j];
-		printf("选手排序:");
-	 line(b);
-	 		printf("评委排序:");
-	line(d);
-	printf("\n选手小结");
-	 for(i=0;i<N;i+=find?find:1)
-	{
-		 find=0;
-
-		for(j=0;j<N;j++)
-		{
-			if(b[i]==c[j]) 
-			{
-				if(find++==0)	printf("第%d名为%d号选手 %3.2f分\n",i+1,j+1,b[i]);
-				else 	printf("并列第%d名为%d号选手 %3.2f分\n",i+1,j+1,b[i]);
-			}
-		}
-	}
-	 find=0;
-	 	printf("\n评委小结");
-	 	 for(i=M-1;i>=0;i-=find?find:1)
-	{
-		 find=0;
-
-		for(j=0;j<M;j++)
-		{
-			if(d[i]==e[j]) 
-			{
-				if(find++==0)	printf("第%d名为%d号评委,相差%3.2f分\n",M-i,j+1,d[i]);
-				else 	printf("并列第%d名为%d号评委,相差%3.2f分\n",M-i,j+1,d[i]);
-			}
-		}
-	}
-	return 0;
-}
-float ave(int a[M])
-{
-	int i,max,min;
-	float count=0;
-	max=a[0];
-	min=a[0];
-	for(i=0;i<M;i++)
-	{
-		count=count+a[i];
-		if(a[i]>max) max = a[i];
-		if(a[i]<min) min = a[i];
-	}
-	//printf("		%f\n",count);
-	count-=max+min;
-	count=count/(float)(M-2);
-	return count;
-}
-float line(float b[])
-{
-	int i,j,n;
-	float t;
-	n=sizeof(b);
-	for(i=0;i<n-1;i++)
-	{
-		for(j=i+1;j<n;j++)
-		{
-			if(b[i]<b[j]) 
-			{
-				t=b[i];
-				b[i]=b[j];
-				b[j]=t;
-			}
-		}
-	}
-		 for(i=0;i<n;i++)
-		 printf("%3.2f ",b[i]);
-	 printf("\n");
-	return 0;
-}
-*/
